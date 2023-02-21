685d1df0d54cf94af34831bdd9a2515f846abb4a
Krishna <kpanchap@uw.edu>
2023-02-21 14:44:40 -0800

    initial commit of backend schema

---
+++ server/src/socket/utils.py
@@ -0,0 +1,25 @@
+from fastapi import WebSocket, status, Query
+from typing import Optional
+from ..socket.connection import ConnectionManager
+
+from ..redis.producer import Producer
+from ..redis.config import Redis, Path
+from ..schema.chat import Chat, Message
+
+from ..redis.config import Redis
+
+async def get_token(
+    websocket: WebSocket,
+    token: Optional[str] = Query(None),
+):
+    redis = Redis()
+    if token is None or token == "":
+        await websocket.close(code=status.WS_1008_POLICY_VIOLATION)
+
+    redis_client = await redis.create_connection()
+    isexists = await redis_client.exists(token)
+
+    if isexists == 1:
+        return token
+    else:
+        await websocket.close(code=status.WS_1008_POLICY_VIOLATION, reason="Session not authenticated or expired token")
---
+++ worker/src/model/gpt.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+import requests
+import json
+
+load_dotenv()
+
+# class GPT():
+#     self.key = os.environ.get('OPEN_API_KEY')
